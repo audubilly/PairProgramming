c838ef5940fbcd721fa9bab154a0419d18050573
Onabanjo Titus <74909457+OnabanjoTitus@users.noreply.github.com>
2021-02-09 15:58:03 +0100

    first commit

---
+++ BankAccountTest.java
@@ -0,0 +1,18 @@
+package BankingApplication;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+    BankAccount bankAccount;
+    @BeforeEach
+    void setUp() {
+        bankAccount= new BankAccount();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ CustomerTest.java
@@ -0,0 +1,18 @@
+package BankingApplication;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+    Customer customer;
+    @BeforeEach
+    void setUp() {
+        customer= new Customer();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
